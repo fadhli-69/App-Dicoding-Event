5303eaeb7f42ef619b1bb7679e127d5e294bd783
Muhammad Nurfadhli <79645149+mozart-69@users.noreply.github.com>
2024-10-28 09:01:06 +0700

    menambahkan fitur reminder

---
+++ app/src/main/java/com/dicoding/aplikasidicodingevent/data/preferences/ReminderPreference.kt
@@ -0,0 +1,4 @@
+package com.dicoding.aplikasidicodingevent.data.preferences
+
+class ReminderPreference {
+}
---
+++ app/src/main/java/com/dicoding/aplikasidicodingevent/worker/ReminderWorker.kt
@@ -0,0 +1,4 @@
+package com.dicoding.aplikasidicodingevent.worker
+
+class ReminderWorker {
+}
